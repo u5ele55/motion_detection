5578dfa13a2df17d2ef18470244327610affe70a
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-07-14 16:04:06 +0300

    method to check if point is inside rectangle added

--- src/utilities/geometry.py
+++ src/utilities/geometry.py
@@ -10,3 +10,10 @@ class Geometry:
         x1,y1, x2,y2 = small_rect
 
         return X1 <= x1 and x2 <= X2 and Y1 <= y1 and y2 <= Y2
+    @staticmethod
+    def inside(point: list | np.ndarray, rect: np.ndarray) -> bool:
+        '''Checks if `point` is inside rectangle `rect`'''
+        x, y = point
+        x1,y1, x2,y2 = rect
+
+        return x1 <= x <= x2 and y1 <= y <= y2
