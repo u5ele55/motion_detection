1d8b68372f1ac23365c3b7aff6ca6aaa14cc196e
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-07-17 20:47:31 +0300

    some ideas added

--- src/motion_detection/no_opencv/MotionDetector.py
+++ src/motion_detection/no_opencv/MotionDetector.py
@@ -4,6 +4,9 @@ from utilities.geometry import *
 
 class CVMotionDetector(IMotionDetector):
     def __init__(self, *, min_height: int=12, min_width: int=12, ):
+        # We'll use mean of sequence of frames as value to which current frame is compared. 
+        # Also we'll use binarization of their difference with big threshold, as noise couldn't 
+        # be deleted with filtering
         pass
 
     def detect_motion(self, frame: np.ndarray, return_processed_frame: bool = False):
