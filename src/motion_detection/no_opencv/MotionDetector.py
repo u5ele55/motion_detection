05034b4c27024cd542f2c5cf271aca12d040f8e4
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-07-21 11:44:40 +0300

    added algorithm parameters to constructor

--- src/motion_detection/no_opencv/MotionDetector.py
+++ src/motion_detection/no_opencv/MotionDetector.py
@@ -7,12 +7,34 @@ import cv2
 class CustomMotionDetector(IMotionDetector):
     def __init__(self, *, 
                  capacity: int=10, 
+                 
+                 max_deviation: int = 0, 
+                 patience: int = 2,
+                 max_elapsed_time: int = 5,
+
+                 move_threshold: int = 30,
+                 object_threshold: int = 5,
+
+                 detection_step: tuple[int]=(10,10),
+                 object_selection_step: tuple[int]=(3,3)
                  ):
 
+        self.capacity = capacity
         self.frame_pool = deque([])
         self.pool_sum = None
 
-        self.capacity = capacity
+        self.figures = {}    # {rect_id: [vertices]}
+        self.figure_center_streak = {} # {rect_id: [center_streak, elapsed_time, found_on_frame]}
+        self.figure_cnt = 0
+
+        self.max_deviation = max_deviation
+        self.patience = patience
+        self.max_elapsed_time = max_elapsed_time
+
+        self.move_threshold = move_threshold
+        self.object_threshold = object_threshold
+        self.detection_step = detection_step
+        self.object_selection_step = object_selection_step
 
     def detect_motion(self, frame: np.ndarray, return_processed_frame: bool = False):
         if self.pool_sum is None:
@@ -40,9 +62,8 @@ class CustomMotionDetector(IMotionDetector):
         return contours
     
     def __grid_contour_search(self, frame):
-        threshold = 30
-        step_y = 10
-        step_x = 10
+        step_y = self.detection_step[1]
+        step_x = self.detection_step[0]
 
         H,W = frame.shape[:2]
 
@@ -50,7 +71,7 @@ class CustomMotionDetector(IMotionDetector):
 
         for row in range(step_y, H, step_y):
             for col in range(step_x, W, step_x):
-                if frame[row, col] >= threshold:
+                if frame[row, col] >= self.move_threshold:
                     # ignore pixels inside of already created rectangles
                     if self.__insideContour(contours, row, col):
                         continue
@@ -66,9 +87,8 @@ class CustomMotionDetector(IMotionDetector):
     def __inflateRectangle(self, frame, start_y, start_x):
         '''Starting at (x,y), tries to find inner points of figure with BFS, and then fits it into rectangle'''
         rect = [start_x,start_y,start_x,start_y]
-        step_y = 3
-        step_x = 3
-        threshold = 5
+        step_y = self.object_selection_step[1]
+        step_x = self.object_selection_step[0]
 
         H,W = frame.shape[:2]
 
@@ -97,7 +117,7 @@ class CustomMotionDetector(IMotionDetector):
 
             absolute_y, absolute_x = start_y % step_y + y * step_y, start_x % step_x + x * step_x
 
-            if frame[absolute_y, absolute_x] > threshold:
+            if frame[absolute_y, absolute_x] > self.object_threshold:
                 in_figure = True
                 # update rect
                 if absolute_x < rect[0]:
