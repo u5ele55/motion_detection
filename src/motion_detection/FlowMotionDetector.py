1fe227aea80e91b683eef7f91adf8fc76b94872e
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-08-07 20:54:20 +0300

    grid step added to constructor arguments

--- src/motion_detection/FlowMotionDetector.py
+++ src/motion_detection/FlowMotionDetector.py
@@ -8,7 +8,17 @@ warnings.filterwarnings("error")
 MAX_FRAME_RESOLUTION_FLOW = 130_000
 
 class FlowMotionDetector(IMotionDetector):
-    def __init__(self, sample_frame: np.ndarray, patience: int=2):
+    def __init__(self, sample_frame: np.ndarray, *, 
+                 grid_shape: tuple[int]=(5,5),
+                 ):
+        '''
+        
+        Parameters
+        ----------
+        grid_shape: tuple[int]
+            step on x and y-axis when iterating through flow frame in search of figure
+        
+        '''
         self.new_size = None
 
         H, W = sample_frame.shape[:2]
@@ -25,6 +35,8 @@ class FlowMotionDetector(IMotionDetector):
         self.previous = cv2.cvtColor(sample_frame, cv2.COLOR_BGR2GRAY)
         self.hsv = np.zeros_like(sample_frame)
         self.hsv[:, :, 1] = 255
+
+        self.grid_shape = grid_shape
     
     def detect_motion(self, frame: np.ndarray, return_processed_frame=False):
         if self.new_size is not None: 
@@ -49,6 +61,12 @@ class FlowMotionDetector(IMotionDetector):
         H, W = flow.shape[:2]
         
         # perform grid checking looking for pixels that arent close to estimated pixel (which could be calculated as mean of previous pixels or mean of some area around current pixel)
+        step_x, step_y = self.grid_shape
+
+        for y in range(0, H, step_y):
+            for x in range(0, W, step_x):
+                # calculate mean and compare to current pixel
+                pass
 
     def __color_diff(self, c1, c2):
         ''' colors in bgr '''
@@ -56,10 +74,8 @@ class FlowMotionDetector(IMotionDetector):
         c2 = c2.astype('int32')
         rmean = ( c1[2] + c2[2] ) / 2
 
-        try:
-            b = c1[0] - c2[0]
-            g = c1[1] - c2[1]
-            r = c1[2] - c2[2]
-        except:
-            print(c1, c2)
+        b = c1[0] - c2[0]
+        g = c1[1] - c2[1]
+        r = c1[2] - c2[2]
+        
         return np.sqrt((((512+rmean)*r*r)/256) + 4*g*g + (((767-rmean)*b*b)/256))
